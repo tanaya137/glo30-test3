2595de70dbaba93645a3dcbe6e4390b28b7599f6
tanaya137 <tanaya137@gmail.com>
2023-04-03 02:48:17 +0300

    hi

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+router.get('/demo', requiresAuth(), function (req, res, next) {
+  res.render('demo', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Demo page'
+  });
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
+});
+
+module.exports = router;
